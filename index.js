067307a5a9b3b4ce6eae634445e087f0182525cf
cc-takahashi <takahashikenichi@colorfulcompany.co.jp>
2018-07-19 16:13:47 +0900

    a

--- index.js
+++ index.js
@@ -485,9 +485,9 @@ exports.afterStoredConversion = (event, callback) => {
       })
       .catch(err => {
         console.error('ERROR:', err);
-        console.error('ERROR:', err.KintoneAPIException);
+        console.error('ERROR:', err.indexOf('KintoneAPIException'));
         console.error('ERROR:', err.get());
-        if (err instanceof KintoneAPIException) {
+        if (err.indexOf('KintoneAPIException') !== -1) {
           const kintoneErr = err.get();
           if (kintoneErr.errors[record[conversion_id].value].message === '値がほかのレコードと重複しています。') {
             return updateStatus(sf, contents, 'complete');
